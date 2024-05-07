9d1f41e140cd7b18ac2982a53a7c5c717b6dfa93
Tung Dinh <tungxd301@gmail.com>
2024-05-06 23:47:32 -0700

    Changes

--- src/layoutComponents/Container.tsx
+++ src/layoutComponents/Container.tsx
@@ -4,10 +4,7 @@ export const Container = (props: { children?: ReactNode }) => {
   return (
     <div>
       <div className="h-14" />
-      <div className="flex">
-        <div className="w-72" />
-        <div className="pl-72 pr-24">{props.children}</div>
-      </div>
+      <div className="ml-72 px-24">{props.children}</div>
     </div>
   );
 };
