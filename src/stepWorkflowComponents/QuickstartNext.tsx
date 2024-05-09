4c3af07c585e463b5b375db353527322943b04f8
Tung Dinh <tungxd301@gmail.com>
2024-05-09 14:09:44 -0700

    Changes

--- src/stepWorkflowComponents/QuickstartNext.tsx
+++ src/stepWorkflowComponents/QuickstartNext.tsx
@@ -1,7 +1,13 @@
+import { Typography } from "antd";
+
 export const QuickstartNext = () => {
   return (
-    <div>
-      <h1>Coming soon...</h1>
-    </div>
+    <Typography.Paragraph>
+      <Typography.Paragraph>
+        Great, you're now set up with an API client and have made your first
+        request to the API. Here are a few links that might be handy as you
+        venture further into the Step Workflow API:
+      </Typography.Paragraph>
+    </Typography.Paragraph>
   );
 };
